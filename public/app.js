80036feb8d5eca200fe800e24f158fb27ccab8af
Matt Dudzik <mdudzik92@gmail.com>
2020-11-24 03:03:14 -0600

    Updated handlebars files for a working landing page

--- public/app.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $(".burger").on("click", function(){
-        var burgerId = $(this).data("id")
-        console.log(burgerId);
-    })
-})
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,46 @@
+$(function(){
+    $(".change-burger").on("click", function(event){
+        var burgerId = $(this).data("id");
+        var newDevoured = $(this).data("newdevoured");
+
+        var newDevouredState = {
+            devoured: newDevoured
+          };
+  
+      // Send the PUT request.
+      $.ajax("/api/burgers/" + burgerId, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(
+        function() {
+          console.log("changed devoured to", newDevoured);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newBurger = {
+        name: $("#ca").val().trim(),
+        devoured: $("[name=devoured]:checked").val()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+  });
+  
